0c17e581556862d12a2764fb4ddaf02ca595b306
Stefan Feiler <stefantfeiler@gmail.com>
2023-05-19 14:42:09 -0600

    for corey

--- PageRank.py
+++ PageRank.py
@@ -3,101 +3,122 @@ import os
 from sparse_matrix import SparseMatrix
 
 
-def page_rank(folder, links, n):
+class PageRank():
+    def __init__(self, folder, links, n):
+        self.folder = folder
 
-    def get_top_rows_by_nonzero_count(array, n):
+        self.links = links
+
+        self.n = n
+
+        self.M = self.make_initial_matrix()
+
+        self.convergence_iterations = 6
+
+    def make_initial_matrix(self):
+
+        terms_to_article_names = {}
+
+        all_art_names = os.listdir(self.folder)[:self.n]
+
+        # Get the length of the actual list of articles
+
+        for test_file in all_art_names:
+
+            content = open(f"{self.folder}/" + test_file).read().replace("\n", " ").lower().split(" ")
+
+            for term in set(content):
+
+                if term in terms_to_article_names:
+
+                    terms_to_article_names[term].add(test_file)
+
+                else:
+
+                    terms_to_article_names[term] = {test_file}
+                # terms_to_article_names[term] = list(set(terms_to_article_names[term]))
+
+        all_links = np.genfromtxt(
+            self.links,
+            dtype="str",
+            delimiter="\t",
+            skip_header=11
+        )
+
+        M = np.eye(len(all_art_names))
+
+        for pair in all_links:
+            first_page, second_page = pair
+            if first_page + ".txt" in all_art_names and second_page + ".txt" in all_art_names:
+                index_1 = all_art_names.index(first_page + ".txt")
+                index_2 = all_art_names.index(second_page + ".txt")
+
+                M[index_1, index_2] += 1
+
+        for i in range(M.shape[0]):
+            M[i] /= M[i].sum()
+
+        return M
+
+    def get_top_rows_by_nonzero_count(self, n):
+
+        array = self.M
+
         row_indices = np.arange(len(array))
 
         nonzero_counts = np.count_nonzero(array, axis=1)
 
-        sorted_indices = np.argsort(nonzero_counts)[::-1] # Sort indices in descending order
+        sorted_indices = np.argsort(nonzero_counts)[::-1]  # Sort indices in descending order
 
         top_indices = sorted_indices[:n]  # Select the top n indices
 
         return array[row_indices[top_indices], :]
 
-    num_articles = 100
+    def do_multiplication_6_times(self, matrixClass):
 
-    terms_to_article_names = {}
+        M = matrixClass(self.M)
 
-    # all_art_names = os.listdir(folder)[:num_articles]
-    all_art_names = os.listdir(folder)
+        M2 = matrixClass(self.M)
 
-    # all_art_names = os.listdir(folder)
+        i = 0
 
-    # Get the length of the actual list of articles
+        total = 0
 
-    for test_file in all_art_names:
+        #counter = self.convergence_iterations
+        counter = 6
+        while i < counter:
 
-        content = open(f"{folder}/" + test_file).read().replace("\n", " ").lower().split(" ")
+            M2, count = M2 * M
 
-        for term in set(content):
+            total += count
 
-            if term in terms_to_article_names:
+            i += 1
 
-                terms_to_article_names[term].add(test_file)
+        return M, total
 
-            else:
+    def find_convergence(self):
+        # Get the amount of articles (amount of rows and columns in our matrix)
+        length = self.n
 
-                terms_to_article_names[term] = {test_file}
-            # terms_to_article_names[term] = list(set(terms_to_article_names[term]))
+        # Make a random array which is that length
+        v = np.random.random(length)
 
-    all_links = np.genfromtxt(
-        links,
-        dtype="str",
-        delimiter="\t",
-        skip_header=11
-    )
+        counter = 1
 
-    M = np.eye(len(all_art_names))
+        tolerance = 0.01
 
-    for pair in all_links:
-        first_page, second_page = pair
-        if first_page + ".txt" in all_art_names and second_page + ".txt" in all_art_names:
-            index_1 = all_art_names.index(first_page + ".txt")
-            index_2 = all_art_names.index(second_page + ".txt")
+        while True:
+            new_v = np.dot(v, self.M)
 
-            M[index_1, index_2] += 1
+            dif = np.abs(new_v - v)
 
-    for i in range(M.shape[0]):
-        M[i] /= M[i].sum()
+            max_dif = np.mean(dif)
 
-    M = get_top_rows_by_nonzero_count(M, n)
-    M = SparseMatrix(M)
-    # Get the amount of articles (amount of rows and columns in our matrix)
-    length = 4604
+            if max_dif < tolerance:
+                break
 
-    # Make a random array which is that length
-    #v = np.random.random(length)
+        v = new_v.copy()
 
-    #counter = 1
+        counter += 1
 
-    #tolerance = 0.01
-
-    #while True:
-        #new_v = np.dot(v, M)
-
-        #dif = np.abs(new_v - v)
-
-        #max_dif = np.mean(dif)
-
-        #if max_dif < tolerance:
-            #break
-
-        #v = new_v.copy()
-
-        #counter += 1
-    
-    i = 0
-    total = 0
-    counter = 6
-    M2 = M.copy()
-    while i < counter:
-        M2,count = M2*M
-        total += count
-        i += 1
-
-
-    #print(f"Convergence achieved after {counter} iterations.")
-
-    return total
+        return counter
--- main.py
+++ main.py
@@ -1,92 +1,108 @@
 from naive_matrix import NaiveMatrix
 from sparse_matrix import SparseMatrix
 
-from PageRank import page_rank
+from PageRank import PageRank
 import matplotlib.pyplot as plt
+
+import time
+
+def test():
+
+    start = time.time()
+
+    pagerank = PageRank("all_wiki", "links.tsv", 300)
+
+    pagerank.make_initial_matrix()
+
+    end_1 = time.time()
+
+    print(f'Time to initialize: {end_1-start}')
+
+    # pagerank.find_convergence()
+    end_2 = time.time()
+    # print(f'Time to find convergence: {end_2-end_1}')
+
+    M, total = pagerank.do_multiplication_6_times(SparseMatrix)
+    end_3 = time.time()
+    print(f'Time to multiply 6 times: {end_3 - end_2} and count: {total}')
+
+    print(pagerank.M)
+
+
 
 
 if __name__ == '__main__':
-    arr1 = [
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
-    ]
 
-    arr2 = [
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
-    ]
-
-    mat1 = SparseMatrix(arr1)
-    mat2 = SparseMatrix(arr2)
-
-    #mat3 = NaiveMatrix(arr1)
-    #mat4 = NaiveMatrix(arr2)
-
-    #res_mat,count_naive = mat3*mat4
-
-    #rslt= NaiveMatrix(res_mat)
-
-    #print(rslt)
-    res_mat2, count_sparse = mat1 * mat2
+    test()
 
 
-
-
-    for i in range(10):
-        print(mat2.sparse)
-        res_mat2,count_sparse = res_mat2*mat2
-
-        print(res_mat2.to_string())
-
-    #print(count_naive,count_sparse)
-
-    M = page_rank("all_wiki", "links.tsv")
-
-    sparse_m = SparseMatrix(M)
-
-    total = 0
-    y = []
-    x = [1,2,3,4,5,6,7,8,9]
-
-    print(len(M))
-
-    import matplotlib.pyplot as plt
-    def EXAMPLE_PLOT_INFO():
-        # Sample data
-        x = [1, 2, 3, 4, 5]
-        y1 = [2, 4, 6, 8, 10]
-        y2 = [1, 3, 5, 7, 9]
-        labels = ['Data 1', 'Data 2']
-
-        # Scatter plots
-        plt.scatter(x, y1, label=labels[0])
-        plt.scatter(x, y2, label=labels[1])
-
-        # Axis labels
-        plt.xlabel('X-axis')
-        plt.ylabel('Y-axis')
-
-        # Title
-        plt.title('Scatter Plot')
-
-        # Legend
-        plt.legend(title='Legend')
-    
-        # Display the plot
-        plt.show()
-
-    for i in range(9):
-        print(f'Starting step: {i}, len: {len(sparse_m.sparse)}')
-        sparse_m, count = sparse_m*sparse_m
-        total += count
-        y.append(total)
-        print(f'Finished step: {i}')
-    print(y)
-
-
-    plt.scatter(x,y)
-    plt.show()
+    # arr1 = [
+    #     [1, 0, 0],
+    #     [0, 1, 0],
+    #     [0, 0, 1]
+    # ]
+    #
+    # arr2 = [
+    #     [1, 0, 0],
+    #     [0, 1, 0],
+    #     [0, 0, 1]
+    # ]
+    #
+    # mat1 = SparseMatrix(arr1)
+    # mat2 = SparseMatrix(arr2)
+    #
+    # res_mat2, count_sparse = mat1 * mat2
+    #
+    # for i in range(10):
+    #     print(mat2.sparse)
+    #     res_mat2,count_sparse = res_mat2*mat2
+    #
+    #     print(res_mat2.to_string())
+    #
+    #
+    # sparse_m = SparseMatrix(M)
+    #
+    # total = 0
+    # y = []
+    # x = [1,2,3,4,5,6,7,8,9]
+    #
+    # print(len(M))
+    #
+    # import matplotlib.pyplot as plt
+    # def EXAMPLE_PLOT_INFO():
+    #     # Sample data
+    #     x = [1, 2, 3, 4, 5]
+    #     y1 = [2, 4, 6, 8, 10]
+    #     y2 = [1, 3, 5, 7, 9]
+    #     labels = ['Data 1', 'Data 2']
+    #
+    #     # Scatter plots
+    #     plt.scatter(x, y1, label=labels[0])
+    #     plt.scatter(x, y2, label=labels[1])
+    #
+    #     # Axis labels
+    #     plt.xlabel('X-axis')
+    #     plt.ylabel('Y-axis')
+    #
+    #     # Title
+    #     plt.title('Scatter Plot')
+    #
+    #     # Legend
+    #     plt.legend(title='Legend')
+    #
+    #     # Display the plot
+    #     plt.show()
+    #
+    # for i in range(9):
+    #     print(f'Starting step: {i}, len: {len(sparse_m.sparse)}')
+    #     sparse_m, count = sparse_m*sparse_m
+    #     total += count
+    #     y.append(total)
+    #     print(f'Finished step: {i}')
+    # print(y)
+    #
+    #
+    # plt.scatter(x,y)
+    # plt.show()
 
 
